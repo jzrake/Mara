ba9f086cbd4bd7c397e78f672f034aaa47ba29a3
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-02-01 23:10:41 -0500

    Updated visual module draws textures from lunum arrays.

---
+++ test/vis.lua
@@ -0,0 +1,16 @@
+
+
+local Nx = 10
+local Ny = 240
+
+local x = 0.04 * lunum.range(Nx) * math.pi
+local y = 0.04 * lunum.range(Ny) * math.pi
+
+local A = lunum.zeros{Nx,Ny}
+
+for i,j in A:indices() do
+   A[{i,j}] = math.sin(x[i]) + math.cos(y[j])
+end
+
+visual.open_window()
+visual.draw_texture(A)
