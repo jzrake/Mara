cf5b671a03d9b6479f556684a961b49456fc2772
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-02-02 11:54:07 -0500

    Have 2d texture drawing working correctly.

--- test/vis.lua
+++ test/vis.lua
@@ -3,14 +3,24 @@
 local Nx = 100
 local Ny = 240
 
-local x = 0.08 * lunum.range(Nx) * math.pi
-local y = 0.04 * lunum.range(Ny) * math.pi
+visual.open_window()
 
-local A = lunum.zeros{Nx,Ny}
+local x = lunum.zeros{Nx,Ny}
+local y = lunum.zeros{Nx,Ny}
 
-for i,j in A:indices() do
-   A[{i,j}] = math.sin(x[i]) + math.cos(y[j])
+for i,j in x:indices() do
+   x[{i,j}] = 0.08 * i * math.pi
+end
+for i,j in y:indices() do
+   y[{i,j}] = 0.04 * j * math.pi
 end
 
-visual.open_window()
-visual.draw_texture(A)
+
+for iter=0,1000 do
+
+   local A = lunum.sin(x+math.cos(0.1*iter)) + lunum.cos(y-math.sin(0.1*iter))
+   print(iter)
+   collectgarbage()
+   visual.draw_texture(A)
+
+end
