7ebff2e4fb1ec8d88de6dd645bf9f86ef8962020
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-02-03 11:07:32 -0500

    Moved Lua code out of sampling.cpp, the call is made from mara.cpp now. sampling.lua test performs correctly on 10,000 points x 16 mpi procs on laptop.

--- test/sampling.lua
+++ test/sampling.lua
@@ -1,11 +1,29 @@
 
 
+-- *****************************************************************************
+--
+-- Test of parallel sampling routines.
+--
+-- *****************************************************************************
+
+local Nsamp = 10000
+local verbose = false
+local start = 
+
 set_domain({0,0,0}, {1,1,1}, {16,16,16}, 8, 2)
-init_prim(function(x,y,z) return lunum.zeros(8)+1 end)
 
-local P = sampling.sample({0.1, 0.1, 0.1})
-print(P)
+init_prim(function(x,y,z) 
+	     local r = (x^2 +y^2 + z^2)^0.5	     
+	     return {1,1, 0,0,0, x/r, y/r, z/r}
+	  end)
 
-for i=0,1000 do
+local start = os.clock()
 
+for i=0,Nsamp do
+   local x, y, z =  math.random(), math.random(), math.random()
+   local P = prim_at_point{x,y,z}
+   if verbose then print(lunum.array{x,y,z}, P) end
 end
+
+print(string.format("took %d samples in %f seconds",
+		    Nsamp, os.clock() - start))
