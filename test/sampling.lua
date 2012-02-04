2a856d3eac1d066c02a78e779f9cf0e9ff79207f
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-02-03 19:33:28 -0500

    Have bilinear interpolation with sampling working in 2d, not tested in 3d or with MPI. Fixed a potential bug where MPI non-blocking sends would have referenced out-of-date data. Exposed bounary.ApplyBoundaries to Lua.

--- test/sampling.lua
+++ test/sampling.lua
@@ -6,12 +6,12 @@
 --
 -- *****************************************************************************
 
-local Nsamp = 0
-local Nzone = 15
-local verbose = true
+local Nsamp = 32000
+local Nzone = 16
 
 
-local function TestSamplingNd(dims)
+
+local function TestSamplingNd(dims, mode, verbose)
 
    if     dims == 1 then
       set_domain({0}, {1}, {Nzone}, 8, 2)
@@ -23,33 +23,68 @@ local function TestSamplingNd(dims)
       set_domain({0,0,0}, {1,1,1}, {Nzone,Nzone,Nzone}, 8, 2)
    end
 
-   local coords = { }
+
+   local coords = { } -- buffer the grid points sampled by init_prim
+
    init_prim(function(x,y,z)
 		table.insert(coords, lunum.array({x,y,z}))
-		if (x == 0.5 and y == 0.5) then return {1,1, 0,0,0, 0,0,0}
-		else
-		   return {1,1, 0,0,0, x,y,z}
-		end
+		return {1,1, 0,0,0, x,y,z}
 	     end)
 
-   visual.draw_texture(get_prim().Bx)
+   set_boundary("outflow")
+   boundary.ApplyBoundaries()
 
-   local start = os.clock()
+   if mode == 'random' then
+      -- ***********************************************************************
+      -- Choose random points to be sampled throughout the domain
 
-   for i=0,Nsamp do
-      local x, y, z =  math.random(), math.random(), math.random()
-      local P = prim_at_point{x,y,z}
-      if verbose then print(lunum.array{x,y,z}, P) end
+
+      local start = os.clock()
+
+      for i=0,Nsamp do
+	 local x, y, z =  math.random(), math.random(), math.random()
+	 local P = prim_at_point{x,y,z}
+	 if verbose then print(lunum.array{x,y,z}, P) end	
+      end
+      
+      print(string.format("took %d samples in %f seconds",
+			  Nsamp, os.clock() - start))
+
+
+   elseif mode == 'grid' then
+      -- ***********************************************************************
+      -- Run the samples over the same points sampled by the init_prim function
+
+      for k,v in pairs(coords) do
+	 local P = prim_at_point(v)
+	 if verbose then print(v,P) end
+      end
+
+      visual.open_window()
+      visual.draw_texture(get_prim().Bx)
+
+
+   elseif mode == 'prolong' then
+      -- ***********************************************************************
+      -- Sample the grid one level finer to test the quality of interpolation
+
+      local Bx = lunum.zeros{2*Nzone, 2*Nzone}
+
+      for i,j in Bx:indices() do
+	 local x = (i + 0.5) / (2*Nzone)
+	 local y = (j + 0.5) / (2*Nzone)
+	 print(x,y)
+	 Bx[{i,j}] = prim_at_point{x,y}[6]
+      end
+
+      visual.open_window()
+      visual.draw_texture(Bx)
    end
-   for k,v in pairs(coords) do
-      local P = prim_at_point(v)
-      if verbose then print(v,P) end
-   end
-
-   print(string.format("took %d samples in %f seconds",
-		       Nsamp, os.clock() - start))
 end
 
+
 set_fluid("rmhd")
-visual.open_window()
-TestSamplingNd(2)
+
+--TestSamplingNd(3, 'random', false)
+--TestSamplingNd(2, 'grid', false)
+TestSamplingNd(2, 'prolong', false)
