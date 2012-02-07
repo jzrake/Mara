c9941f58a0e60b91b4efb3d3d16c59bce32499b2
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-02-07 00:27:10 -0500

    Simplified build process a bit. conf directory is now a Python module.

---
+++ conf/settings.py
@@ -0,0 +1,15 @@
+
+
+
+
+SystemConfig = { }
+
+SystemConfig["cflags"]  =  "-Wall"
+SystemConfig["clibs"]   =  "-lreadline -lncurses"
+SystemConfig["cc"]      =  "cc"
+SystemConfig["cxx"]     =  "c++"
+SystemConfig["mpi"]     =  False
+SystemConfig["gl"]      =  False
+SystemConfig["hdf5"]    =  "/usr"
+SystemConfig["fftw"]    =  "/usr"
+
