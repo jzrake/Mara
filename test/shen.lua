d9cc9d1c0dc60e4e5824ca80d6480804ccf1c40f
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-03-01 11:43:30 -0500

    Some maintenance on Shen. Presently the direct lookup is enabled which makes things a little faster. Consider this commit suspect if things go wrong in coming runs.

--- test/shen.lua
+++ test/shen.lua
@@ -4,12 +4,14 @@ json = require 'json'
 host = require 'host'
 
 local shen_file = host.ShenFile
-tab = load_shen(shen_file, 0.08, {1e12, 1e16}, {0.1, 200.0})
+tab = load_shen(shen_file, 0.08, {1e12, 1e14}, {10.0, 100.0})
 
---f = io.open("shen_08.json", "r")
---contents = f:read("*all")
---tab = json.decode(contents)
+for k,v in pairs(tab) do
+--   print (k,v:shape('array'))
+--   print(v)
+end
 
 set_eos("shen", tab)
-
 test_shen()
+
+
