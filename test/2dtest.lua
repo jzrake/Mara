17196c99dcdd0a7b18e5f175d53b7d4d4012839a
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-05-08 14:11:21 -0400

    SR eigenvectors also work with rotation matrices. WENO symmetry tested and passed, but no SR convergence tests are written yet.

--- test/2dtest.lua
+++ test/2dtest.lua
@@ -86,9 +86,9 @@ function ExplosionRmhd(x,y,z)
 end
 
 
-set_domain({-0.5, -0.5, -0.5}, {0.5, 0.5, 0.5}, {16,16,16}, 5, 3)
---set_domain({-0.5, -0.5}, {0.5, 0.5}, {16, 16}, 5, 3)
-set_fluid("euler")
+set_domain({-0.5, -0.5, -0.5}, {0.5, 0.5, 0.5}, {16,16,16}, 5, 7)
+--set_domain({-0.5, -0.5}, {0.5, 0.5}, {16, 16}, 5, 7)
+set_fluid("srhd")
 set_eos("gamma-law", 1.4)
 --set_boundary("reflect2d", 2, 3)
 set_boundary("periodic")
