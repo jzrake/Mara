0b926fe898cc26954e009d6bc9c5a79a5e870a1a
Bez <darien0@gmail.com>
2012-05-09 21:26:28 -0400

    Prepping for merging 1d and 2d test files

--- test/1dtest.lua
+++ test/1dtest.lua
@@ -34,44 +34,6 @@ local function HandleErrors(Status, attempt)
    return 0
 end
 
--- *****************************************************************************
--- Main driver, operates between checkpoints and then returns
--- .............................................................................
-local function RunSimulation(Status, Howlong)
-
-   local t0 = Status.CurrentTime
-   local attempt = 0
-
-   while Status.CurrentTime - t0 < Howlong do
-
-      if HandleErrors(Status, attempt) ~= 0 then
-         return 1
-      end
-      attempt = attempt + 1
-
-      local dt = Status.Timestep
-      local kzps, errors = advance(dt)
-
-      if errors == 0 then
-         driving.Advance(dt)
-         driving.Resample()
-
-	 if not Quiet then
-	    print(string.format("%05d(%d): t=%5.4f dt=%5.4e %3.2fkz/s %3.2fus/(z*Nq)",
-				Status.Iteration, attempt-1, Status.CurrentTime, dt,
-				kzps, 1e6/8/(1e3*kzps)))
-	    io.flush()
-	 end
-
-         attempt = 0
-         Status.Timestep = get_timestep(RunArgs.CFL)
-         Status.CurrentTime = Status.CurrentTime + Status.Timestep
-         Status.Iteration = Status.Iteration + 1
-      end
-   end
-   return 0
-end
-
 local Euler1dProblems = {
 
    Shocktube1 = {
@@ -152,34 +114,6 @@ local Rmhd1dProblems = {
    },
 }
 
-local function InitSimulation(pinit, setup)
-
-   if not Quiet then
-      print("runtime arguments:")
-      for k,v in pairs(RunArgs) do
-	 print(k,v)
-      end
-   end
-
-   setup()
-   init_prim(pinit)
-   boundary.ApplyBoundaries()
-
-   local Status = { }
-
-   Status.CurrentTime = 0.0
-   Status.Iteration   = 0
-   Status.Checkpoint  = 0
-   Status.Timestep    = 0.0
-
-   local datadir = string.format("data/%s", RunArgs.id)
-   os.execute(string.format("mkdir -p %s", datadir))
-   os.execute(host.Filesystem(datadir))
-
-   if not Quiet then print_mara() end
-   return Status
-end
-
 local function setup()
    local N = RunArgs.N
    local NumberOfGhosts = { rmhd=8, srhd=5, euler=5 }
@@ -195,10 +129,8 @@ end
 
 local function CompareWenoEuler()
 
--- local problem = tests.MakeShocktubeProblem(tests.SrhdCase1_DFIM98)
    local problem = tests.MakeShocktubeProblem(Euler1dProblems.Shocktube1, {reverse=false})
-   local Status = InitSimulation(problem:get_pinit(), setup)
-   RunSimulation(Status, RunArgs.tmax)
+   util.run_simulation(problem:get_pinit(), setup , RunArgs)
 
    local P = get_prim()
 
@@ -211,8 +143,7 @@ local function IsentopicConvergenceRate()
 
    RunArgs.boundary = "periodic"
 
-   local Status = InitSimulation(Euler1dProblems.IsentropicPulse.pinit, setup)
-   RunSimulation(Status, RunArgs.tmax)
+   util.run_simulation(Euler1dProblems.IsentropicPulse.pinit, setup , RunArgs)
 
    local P = get_prim()
    if RunArgs.noplot ~= '1' then
@@ -259,8 +190,7 @@ local function CompareEosRmhd()
       end
    end
 
-   local Status = InitSimulation(pinit, setup)
-   RunSimulation(Status, RunArgs.tmax)
+   util.run_simulation(pinit, setup , RunArgs)
 
    local P = get_prim()
    if RunArgs.noplot ~= '1' then
--- test/2dtest.lua
+++ test/2dtest.lua
@@ -18,7 +18,8 @@ local RunArgs = {
    godunov     = "weno-split",
    reconstruct = "weno5",
    eos         = "gamma-law",
-   adgam       = 1.4
+   adgam       = 1.4,
+   interactive = true
 }
 
 for k,v in pairs(cmdline.opts) do
@@ -34,82 +35,8 @@ local function HandleErrors(Status, attempt)
    return 0
 end
 
--- *****************************************************************************
--- Main driver, operates between checkpoints and then returns
--- .............................................................................
-function RunSimulation(CFL, EndTime)
-
-   visual.open_window()
-
-   local dt = 0.0
-   local Iteration = 0
-   local CurrentTime = 0.0
-   local piter = true
-   local start = os.clock()
-
-   print_mara()
-   init_prim(Explosion)--KelvinHelmoltz)
-
---   local problem = tests.DensityWave
---   problem.velocity = { 1.0, 1.0, 0 }
---   problem.mode = { 1.0, 1.0, 0 }
---   init_prim(tests.DensityWave:get_pinit(0.0))
-
-   while CurrentTime < EndTime do
-
-      local prim = get_prim()
-      local draw_array = prim.rho[':,:']
-      visual.draw_texture(draw_array)
-
---      local ppmname = string.format("images/output-%04d.ppm", Iteration)
---      write_ppm(ppmname, prim["rho"])
---      os.execute("convert -rotate 90 " .. ppmname .. " " .. string.gsub(ppmname, ".ppm", ".png"))
---      os.execute("rm " .. ppmname)
-
-      local kzps = advance(dt)
-
-      if piter then
-         print(string.format("%05d: t=%5.4f, dt=%5.4e %3.2fkz/s",
-                             Iteration, CurrentTime, dt, kzps))
-      end
-
-      dt = get_timestep(CFL)
-      CurrentTime = CurrentTime + dt
-      Iteration = Iteration + 1
-
-      collectgarbage()
-   end
-
-   print(string.format("run took %3.2f seconds", os.clock() - start))
-   return get_prim()
-end
 
 
-function Explosion(x,y,z)
-   local r2 = x*x + y*y + z*z
-   if r2 < 0.005 then
-      return { 1.000, 1.0, 0, 0, 0 }
-   else
-      return { 0.125, 0.1, 0, 0, 0 }
-   end
-end
-
-
-function KelvinHelmoltz(x,y,z)
-
-   if math.abs(y) > 0.25 then
-      rho = 1.0
-      vx = -0.5
-   else
-      rho = 2.0
-      vx =  0.5
-   end
-
-   vx = 0.02*(math.random() - 0.5) + vx
-   vy = 0.02*(math.random() - 0.5)
-
-   return { rho, 2.5, vx, vy, 0.0 }
-end
 
 
 function ExplosionRmhd(x,y,z)
@@ -121,15 +48,18 @@ function ExplosionRmhd(x,y,z)
    end
 end
 
-local N = RunArgs.N
-local NumberOfGhosts = { rmhd=8, srhd=5, euler=5 }
 
-set_domain({-0.5, -0.5, -0.5}, {0.5, 0.5, 0.5}, {N,N,N}, NumberOfGhosts[RunArgs.fluid], 7)
-set_fluid(RunArgs.fluid)
-set_eos(RunArgs.eos,RunArgs.adgam)
-set_boundary(RunArgs.boundary)
-set_advance(RunArgs.advance)
-set_riemann(RunArgs.riemann)
-set_godunov(RunArgs.godunov)
+local function setup()
+   local N = RunArgs.N
+   local NumberOfConserved = { rmhd=8, srhd=5, euler=5 }
+--   set_domain({-0.5, -0.5, -0.5}, {0.5, 0.5, 0.5}, {N,N,N}, NumberOfGhosts[RunArgs.fluid], 7
+  set_domain({-0.5, -0.5}, {0.5, 0.5}, {N,N}, NumberOfConserved[RunArgs.fluid], 3)
+   set_fluid(RunArgs.fluid)
+   set_eos(RunArgs.eos,RunArgs.adgam)
+   set_boundary(RunArgs.boundary)
+   set_advance(RunArgs.advance)
+   set_riemann(RunArgs.riemann)
+   set_godunov(RunArgs.godunov)
+end
 
-RunSimulation(RunArgs.CFL, RunArgs.tmax)
+util.run_simulation(tests.KelvinHelmoltz:get_pinit(), setup , RunArgs)
