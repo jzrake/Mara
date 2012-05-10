ad59cad950a132ffd29a89d10dfa2d358dda6ce7
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-05-09 21:31:24 -0400

    eigensys.lua test is now awesome. It computes the SRHD flux Jacobian numerically, and it used that capability to expose a bug that the SR eigenvectors were transposed with respect to their eigenvalues.

--- test/eigensys.lua
+++ test/eigensys.lua
@@ -3,13 +3,40 @@
 local matrix = require 'matrix'
 local util = require 'util'
 
-local sep = "---------------------------------"
-local function clip(x)
-   if math.abs(x) < 1e-12 then
-      return 0.0
-   else
-      return x
+
+local Direction = 1
+local PrimState = { 1.0, 4.0, 0.1, 0.1, 0.1 }
+local FluidType = "srhd"
+
+
+
+-- .............................................................................
+-- Flux Jacobian for SRHD computed numerically by differencing the fluxes.
+-- .............................................................................
+local function JacobianSrhd(U, n)
+   local eps = 1e-12
+   local J = { }
+
+   for i=0,5-1 do
+      J[i+1] = { }
+      for j=0,5-1 do
+
+         local U0 = U:copy()
+         local U1 = U:copy()
+
+         U0[j] = U0[j] - eps
+         U1[j] = U1[j] + eps
+
+         local P0 = fluid.ConsToPrim(U0)
+         local P1 = fluid.ConsToPrim(U1)
+
+         F0 = fluid.FluxFunction(P0, Direction)
+         F1 = fluid.FluxFunction(P1, Direction)
+
+         J[i+1][j+1] = (F1[i] - F0[i]) / (U1[j] - U0[j])
+      end
    end
+   return matrix(J)
 end
 
 
@@ -17,7 +44,7 @@ end
 -- Flux Jacobian as given by http://www.microcfd.com/download/pdf/aiaa2001-2609.pdf
 -- output is A := dF{D,E,px,py,pz}/d{D,E,px,py,pz}, Mara's convention
 -- .............................................................................
-local function Jacobian1(U, n) -- n must be a unit vector
+local function JacobianEuler(U, n) -- n must be a unit vector
 
    local gm = 1.4
    local g1 = gm - 1.0
@@ -37,27 +64,27 @@ local function Jacobian1(U, n) -- n must be a unit vector
    local a2 = gm*p0/D     -- sound speed
    local h0 = a2 / g1 + ek
 
-   A = { { 0, 0, nx, ny, nz },
-         { (g1*ek-h0)*vn,
-           gm*vn,
-           h0*nx - g1*u*vn,
-           h0*ny - g1*v*vn,
-           h0*nz - g1*w*vn },
-         { g1*ek*nx - u*vn,
-           g1*nx,
-           1*vn - g2*u*nx,
-           u*ny - g1*v*nx,
-           u*nz - g1*w*nx },
-         { g1*ek*ny - v*vn,
-           g1*ny,
-           v*nx - g1*u*ny,
-           1*vn - g2*v*ny,
-           v*nz - g1*w*ny },
-         { g1*ek*nz - w*vn,
-           g1*nz,
-           w*nx - g1*u*nz,
-           w*ny - g1*v*nz,
-           1*vn - g2*w*nz } }
+   local A = { { 0, 0, nx, ny, nz },
+               { (g1*ek-h0)*vn,
+                 gm*vn,
+                 h0*nx - g1*u*vn,
+                 h0*ny - g1*v*vn,
+                 h0*nz - g1*w*vn },
+               { g1*ek*nx - u*vn,
+                 g1*nx,
+                 1*vn - g2*u*nx,
+                 u*ny - g1*v*nx,
+                 u*nz - g1*w*nx },
+               { g1*ek*ny - v*vn,
+                 g1*ny,
+                 v*nx - g1*u*ny,
+                 1*vn - g2*v*ny,
+                 v*nz - g1*w*ny },
+               { g1*ek*nz - w*vn,
+                 g1*nz,
+                 w*nx - g1*u*nz,
+                 w*ny - g1*v*nz,
+                 1*vn - g2*w*nz } }
    return matrix(A)
 end
 
@@ -66,7 +93,7 @@ end
 -- Flux Jacobian as given by Toro (3.79)
 -- output is A := dF{D,E,px,py,pz}/d{D,E,px,py,pz}, Mara's convention
 -- .............................................................................
-local function Jacobian2(U, n) -- n must be {1,0,0}
+local function JacobianToro(U, n) -- n must be {1,0,0}
    local gm = 1.4
    local g1 = gm - 1.0
    local g2 = gm - 2.0
@@ -82,28 +109,40 @@ local function Jacobian2(U, n) -- n must be {1,0,0}
    local a2 = gm*p0/D     -- sound speed
    local H = a2 / g1 + ek
 
-   A = { { 0, 0, 1, 0, 0 },
-         { 0.5*u*(H*(gm-3) + g1*ek - a2), gm*u, H - g1*u^2, -g1*u*v, -g1*u*w },
-         { g1*H - u^2 - a2, g1, (3-gm)*u, -g1*v, -g1*w },
-         { -u*v, 0, v, u, 0 },
-         { -u*w, 0, w, 0, u } }
+   local A = { { 0, 0, 1, 0, 0 },
+               { 0.5*u*(H*(gm-3) + g1*ek - a2), gm*u, H - g1*u^2, -g1*u*v, -g1*u*w },
+               { g1*H - u^2 - a2, g1, (3-gm)*u, -g1*v, -g1*w },
+               { -u*v, 0, v, u, 0 },
+               { -u*w, 0, w, 0, u } }
 
    return matrix(A)
 end
 
 
 local function TestState(P, dim)
+   local sep = "---------------------------------"
+   local thresh = {euler=1e-12, srhd=1e-3}
+   local function clip(x)
+      if math.abs(x) < thresh[FluidType] then
+         return 0.0
+      else
+         return x
+      end
+   end
+
    print("\n\n")
    print(sep)
    print("Testing state on dim:", dim)
    print(sep)
 
+   Jacobians = { euler=JacobianEuler, srhd=JacobianSrhd }
+   set_fluid(FluidType)
+
    local n = { 0,0,0 }
    n[dim] = 1.0
-   local U = fluid.PrimToCons(P)
-   local Lv, Rv, lm = fluid.Eigensystem(P, dim)
 
-   local A = Jacobian1(U, n)
+   local U = fluid.PrimToCons(P)
+   local A = Jacobians[FluidType](U, n)
    local Lv, Rv, lm = fluid.Eigensystem(P, dim)
 
    local L = matrix(Lv)
@@ -120,39 +159,7 @@ local function TestState(P, dim)
    print("\nL.A.R (expect eigenvalues):")
    print(sep)
    print((L*A*R):replace(clip))
-
-   print(sep)
-   print(L)
 end
 
-local function compute_sr_jacobian(P)
-   set_fluid("srhd")
 
-   local eps = 1e-10
-   local U = fluid.PrimToCons(lunum.array(P))
-   local J = lunum.zeros{5,5}
-
-   for i=0,5-1 do
-      for j=0,5-1 do
-
-	 local U0 = U:copy()
-	 local U1 = U:copy()
-
-	 U0[j] = U0[j] - eps
-	 U1[j] = U1[j] + eps
-
-	 local P0 = fluid.ConsToPrim(U0)
-	 local P1 = fluid.ConsToPrim(U)
-
-	 F0 = fluid.FluxFunction(P0, 1)
-	 F1 = fluid.FluxFunction(P1, 1)
-
-	 J[{i,j}] = (F1[i] - F0[i]) / (U1[j] - U0[j])
-      end
-   end
-   return J
-end
-
-local P = { 1, 2, 0.1, 0.1, 0.1 }
-print(compute_sr_jacobian(P))
---TestState(P, 1)
+TestState(PrimState, Direction)
